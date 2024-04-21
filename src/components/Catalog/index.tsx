0f102707180deb3c3986e509b5f9624358cb775e
maxl85 <maxl85@mail.ru>
2024-04-21 12:43:52 +0300

    useState counter

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -7,10 +7,18 @@ import CatalogCard from '../CatalogCard';
 import styles from './Catalog.module.scss';
 
 export default function Catalog() {
+    const [state, setState] = useState(0);
 
     return (
         <div className="container">
             <section className={styles.catalog}>
+
+                
+                <button onClick={() => setState(state - 1)}>-</button>
+                <div>{state}</div>
+                <button onClick={() => setState(state + 1)}>+</button>
+
+
                 <h2 className={styles.catalogTitle}>Выберите пиццу</h2>
                 <ul className={styles.catalogCategory}>
                     <li className={styles.catalogCategoryItem}>Все</li>
