0d6b51c55d57ecc2279eb8783c9e7b36ba09fe3f
Logickoff <SonyaUstinova@gmail.com>
2024-05-13 18:39:02 +0300

    Header1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,15 +9,26 @@ function Header() {
 
     return (
         <header className={styles.header}>
-          <div className={styles['phone-icon']}>
-            <div className={styles['cont-icon']}>
+          <div className ={styles.liner}>
+            <div className={styles['contact-icons']}>
                 <Image src="/assets/icons/Call.svg" width={28} height={28} alt="" />
-                <span className = {styles['number']}>+7 952 552-52-52</span>
+                <span>+7 952 552-52-52</span>
             </div>
-              <div className = {styles['viber']}>
-                <span>Viber </span>
-                <span>Whatsapp Telegram</span>
-              </div>
+         <div className = {styles.mail}>
+            <Image src="/assets/icons/mail.svg" width={28} height={28} alt="" />
+            <span>Mr.Driskell@mail.ru</span>
+         </div>
+         <div className = {styles.logo_glav}>
+          <Image src="/assets/icons/logo.svg" width={186} height={63} alt="" />
+        </div> 
+        <div className = {styles.user_icon}>
+          <div className = {styles.price}>
+            <span>37500</span>
+            <Image src="/assets/icons/bag_headrr.svg" width={28} height={28} alt="" />
+          </div>
+          <Image src="/assets/icons/heart_header.svg" width={28} height={28} alt="" />
+          <Image src="/assets/icons/user_header.svg" width={28} height={28} alt="" />
+        </div>
          </div>
             <div className = {styles['line-icon']}>
               <Image src="/assets/icons/dividers.svg" width={1216} height={10} alt=" " />
