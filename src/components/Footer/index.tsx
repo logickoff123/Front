a48888f69c7ff56d276835cd42ccd1093bb1cb48
Logickoff <SonyaUstinova@gmail.com>
2024-05-19 23:46:15 +0300

    Header+Footer+Cards+Catalog- all static

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,10 +7,32 @@ import styles from './Footer.module.scss'
 
 export default function Footer() {
     return (
-        <div className= {styles.footer}>
-            <div className = {styles.square}>
-                <span>я лох</span>
+        <div className={styles.footer}>
+            <div className={styles.square}>
+                <div className={styles.logo}>
+                    <Image src="/assets/icons/logo.svg" width={186} height={63} alt="" />
+                </div>
+                <div className={styles.contanier1}>
+                    <Link className={styles.textp} href="#"> Промоакции</Link>
+                    <Link className={styles.textp} href="#">Скидкы</Link>
+                    <Link className={styles.textp} href="#">Помощь</Link>
+                </div>
+                <div className={styles.contanier2}>
+                    <Link className={styles.textp} href="#"> О нас</Link>
+                    <Link className={styles.textp} href="#">Контакты</Link>
+                </div>
+                <div className={styles.long}>
+                    <Image src="/assets/icons/long.svg" width={100} height={396} alt="" />
+                </div>
+                <div className={styles.mail}>
+                    <Image src="/assets/icons/mail.svg" width={28} height={28} alt="" />
+                    <span>Mr.Driskell@mail.ru</span>
+                </div>
+                <div className={styles['contact-icons']}>
+                    <Image src="/assets/icons/Call.svg" width={28} height={28} alt="" />
+                    <span>+7 952 552-52-52</span>
+                </div>
             </div>
         </div>
-)
+    )
 }
